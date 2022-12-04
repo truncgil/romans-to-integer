b3b9ba4ceeb17de1cd12a987bc47c5ddf654362f
truncgil <umitreva@gmail.com>
2022-12-04 16:21:44 +0300

    first commit

---
+++ romans_to_integer.php
@@ -0,0 +1,38 @@
+<?php class Solution {
+
+
+/**
+ * @param String $s
+ * @return Integer
+ */
+function romanToInt($string) {
+    $romans = array(
+        'M' => 1000,
+        'CM' => 900,
+        'D' => 500,
+        'CD' => 400,
+        'C' => 100,
+        'XC' => 90,
+        'L' => 50,
+        'XL' => 40,
+        'X' => 10,
+        'IX' => 9,
+        'V' => 5,
+        'IV' => 4,
+        'I' => 1,
+    );
+
+    $roman = $s;
+    $result = 0;
+
+    foreach ($romans as $key => $value) {
+        while (strpos($roman, $key) === 0) {
+            $result += $value;
+            $roman = substr($roman, strlen($key));
+        }
+    }
+    
+
+    return $result;
+}
+} ?>
